83c3bc0efcdb826824ae953e318807ae88b76dc4
kenyk <keny.r@humantech.pe>
2022-10-19 11:36:46 -0500

    perf: minor

--- src/Counter.test.tsx
+++ src/Counter.test.tsx
@@ -2,15 +2,22 @@ import { render, fireEvent, getByTestId } from '@testing-library/react';
 
 import Counter from './Counter';
 
-it('Counter loads with initial state of 0', () => {
-  const { container } = render(<Counter />);
-  const countValue = getByTestId(container, 'btnCounter');
-  expect(countValue.textContent).toBe('count is 0');
-});
+describe('Counter', () => {
+  let counter: HTMLElement;
 
-it('Counter get value when click', () => {
-  const { container } = render(<Counter />);
-  const countValue = getByTestId(container, 'btnCounter');
-  fireEvent.click(countValue);
-  expect(countValue.textContent).toBe('count is 1');
+  beforeEach(() => {
+    const { container } = render(<Counter />);
+    counter = container;
+  });
+
+  it('Counter loads with initial state of 0', () => {
+    const countValue = getByTestId(counter, 'btnCounter');
+    expect(countValue.textContent).toBe('count is 0');
+  });
+
+  it('Counter get value when click', () => {
+    const countValue = getByTestId(counter, 'btnCounter');
+    fireEvent.click(countValue);
+    expect(countValue.textContent).toBe('count is 1');
+  });
 });
